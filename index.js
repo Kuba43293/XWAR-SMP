0378ec7f1f7fe701707ae38ffe25fc9cf7b03a1c
Kuba43293 <Kubap2013@hotmail.com>
2026-02-12 21:18:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.once('ready', () => {
+    console.log(`Zalogowano jako ${client.user.tag}! Bot XWAR SMP jest ONLINE.`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '!hej') {
+        message.reply('Siemanko! Tu bot serwera **XWAR SMP**. Wszystko działa!');
+    }
+});
+
+client.login(process.env.MTQ3MTU0MjA1NDIxODYyOTE0MA.GJbpVb.OIPE9jS6R9kgpW23i-DiNBToN1aFThB6MXnARs);
