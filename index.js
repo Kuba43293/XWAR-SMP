2185f78bf636546216dd8a237ba7bd4b2b43f70e
Kuba43293 <Kubap2013@hotmail.com>
2026-02-13 10:16:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -11,11 +11,21 @@ const client = new Client({
 
 client.once('ready', () => {
   console.log(`Zalogowano jako ${client.user.tag}! Bot XWAR SMP jest ONLINE.`);
+  // Ustawia status bota: "W grze: na XWAR SMP"
+  client.user.setActivity('na XWAR SMP', { type: ActivityType.Playing });
 });
 
 client.on('messageCreate', message => {
+  if (message.author.bot) return;
+
+  // Komenda !hej
   if (message.content === '!hej') {
-    message.reply('Siemanko! Tu bot serwera **XWAR SMP**. Wszystko działa!');
+    message.reply('Siemanko! Tu bot serwera **XWAR SMP**. Wszystko działa! ⚔️');
+  }
+
+  // Komenda !ip oraz !serwer
+  if (message.content === '!ip' || message.content === '!serwer') {
+    message.reply('🎮 **ADRES SERWERA XWAR SMP** 🎮\n\n🌍 IP: `Xwarsmp.aternos.me` \n🔌 Port: `34899` \n\nZasuwaj do gry! 🔥');
   }
 });
 
